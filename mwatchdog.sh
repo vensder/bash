d2c0f88cbb6d5674ada4d70a8d011bc9d270f322
Dmitry Makarov <dma@ggdtech.com>
2015-03-06 16:17:50 +0300

    remove some comments

--- mwatchdog.sh
+++ mwatchdog.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-#PATTERN='/opt/bin/security-manager -d --config=/etc/security_manager/main.cfg'
-# PATTERN='/opt/bin/security-manager -d'
+
 PATTERN='/usr/sbin/snmpd -LS0-6d -Lf'
-# MYPROG="security_manager"
 MYPROG="snmpd"
 SERVICE="/sbin/service"
 PGREP="/usr/bin/pgrep"
