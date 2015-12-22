ad052da525167300d3b760aca320eb4cd91f674d
vensder <vensder@gmail.com>
2015-12-22 10:07:20 +0300

    fix swap create script

--- swap-file.sh
+++ swap-file.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-SWAPDIR=/swap
-SWAPSIZE=1G
+SWAPDIR=/home/ci/swap
+SWAPSIZE=4G
 SWAPFILE=$SWAPSIZE.swap
 
-mkdir -p $SWAPDIR
-fallocate -l $SWAPSIZE $SWAPDIR/$SWAPFILE
-mkswap $SWAPDIR/$SWAPFILE
-chmod 0600 $SWAPDIR/$SWAPFILE
-swapon $SWAPDIR/$SWAPFILE
+if [ ! -f $SWAPDIR/$SWAPFILE ]; then
+  mkdir -p $SWAPDIR
+  fallocate -l $SWAPSIZE $SWAPDIR/$SWAPFILE
+  mkswap $SWAPDIR/$SWAPFILE
+  chmod 0600 $SWAPDIR/$SWAPFILE
+fi
+
+swapon -s | grep $SWAPDIR/$SWAPFILE
+if [ $? -ne 0 ]
+then
+    swapon $SWAPDIR/$SWAPFILE
+fi
 
 #check
 swapon -s
