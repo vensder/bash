465ab7fc8f9bfe2901b076127599511c878de962
vensder <vensder@gmail.com>
2016-06-15 01:26:59 +0300

    shutdown_when_idle.sh added

---
+++ shutdown_when_idle.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Shutdown jenkins slave when slave go to offline due idle
+# For AWS waiting for end of the billing hour (i.e. minutes of uptime)
+
+PATTERN='java -jar slave.jar'
+PGREP="/usr/bin/pgrep"
+
+minutes=$(uptime -p | awk '{print  $(NF-1)}')
+echo $minutes
+
+if [[ "$minutes" -gt 56 ]];
+then
+    $PGREP -f "$PATTERN"
+    # if not running
+    if [[ $? -ne 0 ]]
+    then
+       echo 'we are going to shutdown'
+       sudo shutdown -h now
+    fi
+fi
+
